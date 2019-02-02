52dbbca08a2f2091dc3d4b00705480bb7967b6ac
carlosdelreal <csdelreal@gmail.com>
2019-02-02 17:25:06 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,26 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	state = {
+		isTextVisible: false
+	}
+
+	showHideText = () => {
+		this.setState({
+			isTextVisible: !this.state.isTextVisible
+		});
+	}
+
+	render() {
+		const {isTextVisible} = this.state;
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+				</header>
+				<main>
+					<Welcome
+						text="This is the title"
+						isTextVisible={isTextVisible}
+					/>
+
+					<button onClick={this.showHideText}>Show / Hide</button>
+
+					{isTextVisible &&
+						<p>This should show and hide</p>
+					}
+				</main>
+			</div>
+		);
+	}
+}
+
+class Welcome extends Component {
+	render() {
+		const {text, isTextVisible} = this.props;
+		console.log(isTextVisible);
+
+		return (
+			<h1 className="App-title">{text}</h1>
+		);
+	}
 }
 
 export default App;
